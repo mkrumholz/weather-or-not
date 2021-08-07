bc44152a0bc3f1e752321fb32246ac353e55cf6e
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-07 17:37:11 -0600

    Update open weather facade for timezone offset

--- app/facades/open_weather_facade.rb
+++ app/facades/open_weather_facade.rb
@@ -2,18 +2,19 @@ class OpenWeatherFacade
   def self.forecast(location)
     coordinates = GeocodingFacade.coordinates(location)
     forecast = OpenWeatherService.get_forecast(coordinates[:lat], coordinates[:lon])
+    timezone_offset = forecast[:timezone_offset]
     require 'pry'; binding.pry
-    current = CurrentForecast.new(forecast[:current])
+    current = CurrentForecast.new(forecast[:current], timezone_offset)
     daily = forecast[:daily].first(5).map do |day|
-      DailyForecast.new(day).details
+      DailyForecast.new(day, timezone_offset).details
     end
     hourly = forecast[:hourly].first(8).map do |hour|
-      daily << HourlyForecast.new(hour).details
+      daily << HourlyForecast.new(hour, timezone_offset).details
     end
     {
       current_weather: current,
       daily_weather: daily,
-      hourly_weather: hourly,
+      hourly_weather: hourly
     }
   end
 end
