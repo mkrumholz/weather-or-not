5ad74c2d030d66346289d26bbb2426d842d89905
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-07 18:51:24 -0600

    Refactor OpenWeatherFacade methods

--- app/facades/open_weather_facade.rb
+++ app/facades/open_weather_facade.rb
@@ -2,18 +2,27 @@ class OpenWeatherFacade
   def self.forecast(location)
     coordinates = GeocodingFacade.coordinates(location)
     forecast = OpenWeatherService.get_forecast(coordinates[:lat], coordinates[:lon])
-    timezone_offset = forecast[:timezone_offset]
-    current = CurrentForecast.new(forecast[:current], timezone_offset).details
-    daily = forecast[:daily].first(5).map do |day|
+    tz_offset = forecast[:timezone_offset]
+    {
+      current_weather: current_details(forecast[:current], tz_offset),
+      daily_weather: daily_details(forecast[:daily], tz_offset),
+      hourly_weather: hourly_details(forecast[:hourly], tz_offset)
+    }
+  end
+
+  def self.current_details(current_forecast, timezone_offset)
+    CurrentForecast.new(current_forecast, timezone_offset).details
+  end
+
+  def self.daily_details(daily_forecasts, timezone_offset)
+    daily_forecasts.first(5).map do |day|
       DailyForecast.new(day, timezone_offset).details
     end
-    hourly = forecast[:hourly].first(8).map do |hour|
+  end
+
+  def self.hourly_details(hourly_forecasts, timezone_offset)
+    hourly_forecasts.first(8).map do |hour|
       HourlyForecast.new(hour, timezone_offset).details
     end
-    {
-      current_weather: current,
-      daily_weather: daily,
-      hourly_weather: hourly
-    }
   end
 end
