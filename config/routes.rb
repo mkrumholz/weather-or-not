4649cab47ef7350603b115fa8ac7824dae9829be
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-09 14:08:41 -0600

    Build up to the breweries controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecasts#show'
+      get '/breweries', to: 'breweries#index'
     end
   end
 end
