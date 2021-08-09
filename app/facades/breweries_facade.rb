f7540b5431838318654045664310a7508eb67a15
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-09 15:23:13 -0600

    Refactor BreweriesFacade methods

--- app/facades/breweries_facade.rb
+++ app/facades/breweries_facade.rb
@@ -1,25 +1,30 @@
 class BreweriesFacade
   def self.breweries(location, quantity)
-    coords = GeocodingFacade.coordinates(location)
-    forecast = OpenWeatherService.get_forecast(coords[:lat], coords[:lon])
-    breweries = OpenBreweryService.get_breweries(coords[:lat],coords[:lon],quantity)
-    forecast_details = {
-      summary: forecast[:current][:weather].first[:description],
-      temperature: "#{forecast[:current][:temp].round.to_s} F"
+    coordinates = GeocodingFacade.coordinates(location)
+    local_details = {
+      location: location,
+      forecast: forecast_basics(coordinates),
+      breweries: brewery_list(coordinates, quantity)
     }
-    brewery_list = breweries.map do |brewery|
+    Breweries.new(local_details)
+  end
+
+  def self.brewery_list(coords, quantity)
+    breweries = OpenBreweryService.get_breweries(coords[:lat], coords[:lon], quantity)
+    breweries.map do |brewery|
       {
         id: brewery[:id],
         name: brewery[:name],
         brewery_type: brewery[:brewery_type]
       }
     end
-    local_details = {
-      location: location,
-      forecast: forecast_details,
-      breweries: brewery_list
+  end
+
+  def self.forecast_basics(coordinates)
+    forecast = OpenWeatherService.get_forecast(coordinates[:lat], coordinates[:lon])
+    {
+      summary: forecast[:current][:weather].first[:description],
+      temperature: "#{forecast[:current][:temp].round.to_s} F"
     }
-    # require 'pry'; binding.pry
-    Breweries.new(local_details)
   end
 end
