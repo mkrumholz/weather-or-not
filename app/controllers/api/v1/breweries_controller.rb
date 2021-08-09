0e681ef95948c0e4c35eb83a17a06d72af5a304a
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-09 15:12:49 -0600

    Create Breweries serializer and complete endpoint happy path

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::BreweriesController < ApplicationController
   def index
-    
+    @breweries = BreweriesFacade.breweries(params[:location],params[:quantity])
+    json_response(BreweriesSerializer.new(@breweries))
   end
 end
