289660aeb8ccf9b88008877f1c642a0c33b67d94
Molly Krumholz <mkkrumholz@gmail.com>
2021-08-10 11:39:47 -0600

    Create api key generator

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,4 +2,8 @@ class ApiKey < ApplicationRecord
   validates :token, uniqueness: true, presence: true
 
   belongs_to :user
+
+  def self.generate_new
+    SecureRandom.base64
+  end
 end
--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe ApiKey do
     it {should validate_presence_of :token}
     it {should validate_uniqueness_of :token}
   end
+
+  describe 'class methods' do
+    describe '::generate_new' do 
+      it 'generates a new api key' do
+        key = ApiKey.generate_new
+        expect(key).to be_a String
+      end
+    end
+  end
 end
